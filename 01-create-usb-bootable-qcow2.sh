18f96fb4187ebd2aa073f7ede2b535d48f6521d1
Brandon Geraci <brandon.geraci@gmail.com>
2023-08-21 12:24:58 -0500

    linting

--- 01-create-usb-bootable-qcow2.sh
+++ 01-create-usb-bootable-qcow2.sh
@@ -6,22 +6,21 @@ set -euo pipefail
 USB_NBD="/dev/nbd0"
 USB_EFI_DIR="/mnt/virtual_usb_efi"
 USB_OS_DIR="/mnt/virtual_usb_os"
-GRUB_CMD=""
+
+# Determine if running on Ubuntu or RHEL
+if [[ $(grep -Ei 'debian|ubuntu' /etc/os-release) ]]; then
+    GRUB_CMD="grub-install"
+elif [[ $(grep -Ei 'fedora|redhat' /etc/os-release) ]]; then
+    GRUB_CMD="grub2-install"
+else
+    echo "Unsupported OS. Exiting..."
+    exit 1
+fi
+
 FEDORA_ISO="$HOME/Downloads/Fedora.iso"
 RHEL_QCOW2="$HOME/Downloads/rhel.qcow2"
 RHEL_NBD="/dev/nbd1"
 RHEL_MNT="/mnt/rhel"
-
-# Checking to see if Fedora.iso exists
-if [ ! -f "$FEDORA_ISO" ]; then
-    echo "Fedora ISO not found"
-    exit 1
-fi
-
-if [ ! -f "$RHEL_QCOW2" ]; then
-    echo "RHEL QCOW2 not found"
-    exit 1
-fi
 
 # Logging function to display messages with timestamps
 log() {
@@ -30,8 +29,8 @@ log() {
 
 mount_fedora_images() {
     log "Mounting Fedora images"
-    sudo mkdir -p "/mnt/{fedora,squash,rootfs}"
-    sudo umount -l -f /mnt/{fedora,squash,rootfs}
+    sudo mkdir -p /mnt/fedora /mnt/squash /mnt/rootfs
+    sudo umount -l -f /mnt/fedora /mnt/squash /mnt/rootfs
     sudo mount -o loop $FEDORA_ISO /mnt/fedora
     sudo mount -o loop /mnt/fedora/LiveOS/squashfs.img /mnt/squash
     sudo mount -o loop /mnt/squash/LiveOS/rootfs.img /mnt/rootfs
@@ -49,10 +48,10 @@ load_nbd_module() {
 
 # Cleanup function to ensure no stale mounts or nbd connections
 cleanup() {
-    for device in nbd0 nbd1; do
-        if lsblk | grep -q $device; then
+    for device in $USB_NBD $RHEL_NBD; do
+        if lsblk | grep -q ${device##*/}; then
             log "Cleaning up existing /dev/$device connection..."
-            for partition in $(lsblk -o NAME | grep "^${device}p"); do
+            for partition in $(lsblk -o NAME | grep "^${device##*/}p"); do
                 sudo umount -l /dev/$partition || true
             done
             sudo qemu-nbd --disconnect /dev/$device
@@ -60,7 +59,6 @@ cleanup() {
     done
 }
 
-# Create and set up the loop device
 setup_USB_NBD() {
     sudo qemu-img create -f qcow2 virtual_usb.qcow2 5G
     sudo qemu-nbd --connect=$USB_NBD virtual_usb.qcow2
@@ -82,33 +80,20 @@ setup_USB_NBD() {
 
     sudo e2label ${USB_NBD}p3 MY_LIVE_SYSTEM
 
-    sudo mkdir -p $USB_EFI_DIR
-    sudo mkdir -p $USB_OS_DIR
-    sudo mkdir -p $RHEL_MNT
+    sudo mkdir -p $USB_EFI_DIR $USB_OS_DIR $RHEL_MNT
     sudo mount ${USB_NBD}p1 $USB_EFI_DIR
     sudo mount ${USB_NBD}p3 $USB_OS_DIR
-    # This has to be the second partition. 
-    # The first partition is BIOS BOOT
     sudo mount ${RHEL_NBD}p2 $RHEL_MNT
 }
 
-# Determine the grub command to use and install GRUB
 install_grub() {
-    # Going to take grub files from the fedora iso image.
-
-    sudo cp /mnt/fedora/EFI/* $USB_EFI_DIR
+    sudo cp -r /mnt/fedora/EFI/* $USB_EFI_DIR
     sudo $GRUB_CMD --target=i386-pc --boot-directory=$USB_OS_DIR/boot $USB_NBD
-
 }
 
 create_grub_config() {
     grub_cfg_path="$USB_OS_DIR/boot/grub/grub.cfg"
-
-    # Ensure the directory exists
     sudo mkdir -p "$(dirname "$grub_cfg_path")"
-
-    # Stealing the grub configuration from fedora iso image
-    # and using it for bios mbr grub boot.
     sudo cp /mnt/fedora/EFI/BOOT/grub.cfg $grub_cfg_path
 }
 
@@ -117,37 +102,28 @@ copy_fedora_files() {
     sudo mkdir -p $USB_OS_DIR/images/pxeboot
     sudo cp -r /mnt/fedora/isolinux $USB_OS_DIR
     sudo rm -f $USB_OS_DIR/isolinux/{vmlinuz,initrd.img}
+    
     VMLINUX=$(find $RHEL_MNT/boot/ -type f -name "vmlinuz*" ! -name "*rescue*" | sort -V | tail -n 1)
     INITRAMFS=$(find $RHEL_MNT/boot/ -type f -name "initramfs*" ! -name "*rescue*" ! -name "*boot*" | sort -V | tail -n 1)
 
-    # Check to see if variables VMLINUX and INITRAMFS are empty
-    if [ -z "$VMLINUX" ] || [ -z "$INITRAMFS" ]; then
-        echo "VMLINUX and INITRAMFS are not defined"
-        exit 1
-    fi
+    [[ -z "$VMLINUX" || -z "$INITRAMFS" ]] && { echo "VMLINUX and INITRAMFS are not defined"; exit 1; }
 
     sudo cp "$VMLINUX" $USB_OS_DIR/isolinux/vmlinuz
     sudo cp "$VMLINUX" $USB_OS_DIR/images/pxeboot/vmlinuz
     sudo cp "$INITRAMFS" $USB_OS_DIR/isolinux/initrd.img
     sudo cp "$INITRAMFS" $USB_OS_DIR/images/pxeboot/initrd
-    
 }
 
-
-
-# Final cleanup steps
 final_cleanup() {
     log "Cleaning up..."
     sudo umount $USB_EFI_DIR
     sudo umount $USB_OS_DIR
     sudo qemu-nbd --disconnect $USB_NBD
     sudo modprobe -r nbd
-    sudo umount -l -f /mnt/{fedora,squash,rootfs}
+    sudo umount -l -f /mnt/fedora /mnt/squash /mnt/rootfs
 }
 
-# Main function to coordinate script execution
 main() {
-
     mount_fedora_images
     load_nbd_module
     cleanup
@@ -156,9 +132,6 @@ main() {
     create_grub_config
     copy_fedora_files
     final_cleanup
-
-    log "Script completed successfully!"
 }
 
-# Run the main function
 main
