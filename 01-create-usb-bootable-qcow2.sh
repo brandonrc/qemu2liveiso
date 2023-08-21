a1772cc8fa5d538161fc917bb3c590750db08d49
Brandon Geraci <brandon.geraci@gmail.com>
2023-08-21 09:17:48 -0500

    Updating script

--- 01-create-usb-bootable-qcow2.sh
+++ 01-create-usb-bootable-qcow2.sh
@@ -2,9 +2,39 @@
 
 set -euo pipefail
 
+# Variables
+USB_NBD="/dev/nbd0"
+USB_EFI_DIR="/mnt/virtual_usb_efi"
+USB_OS_DIR="/mnt/virtual_usb_os"
+GRUB_CMD=""
+FEDORA_ISO="$HOME/Downloads/Fedora.iso"
+RHEL_QCOW2="$HOME/Downloads/rhel.qcow2"
+RHEL_NBD="/dev/nbd1"
+RHEL_MNT="/mnt/rhel"
+
+# Checking to see if Fedora.iso exists
+if [ ! -f "$FEDORA_ISO" ]; then
+    echo "Fedora ISO not found"
+    exit 1
+fi
+
+if [ ! -f "$RHEL_QCOW2" ]; then
+    echo "RHEL QCOW2 not found"
+    exit 1
+fi
+
 # Logging function to display messages with timestamps
 log() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
+}
+
+mount_fedora_images() {
+    log "Mounting Fedora images"
+    sudo mkdir -p "/mnt/{fedora,squash,rootfs}"
+    sudo umount -l -f /mnt/{fedora,squash,rootfs}
+    sudo mount -o loop $FEDORA_ISO /mnt/fedora
+    sudo mount -o loop /mnt/fedora/LiveOS/squashfs.img /mnt/squash
+    sudo mount -o loop /mnt/squash/LiveOS/rootfs.img /mnt/rootfs
 }
 
 # Check and load nbd module
@@ -31,48 +61,44 @@ cleanup() {
 }
 
 # Create and set up the loop device
-setup_loop_device() {
+setup_USB_NBD() {
     sudo qemu-img create -f qcow2 virtual_usb.qcow2 5G
-    sudo qemu-nbd --connect=$LOOP_DEVICE virtual_usb.qcow2
+    sudo qemu-nbd --connect=$USB_NBD virtual_usb.qcow2
+    sudo qemu-nbd --connect=$RHEL_NBD "$RHEL_QCOW2"
 
     # Partition the loop device
     log "Partitioning the loop device..."
-    sudo parted -s $LOOP_DEVICE mklabel gpt
-    sudo parted -s $LOOP_DEVICE mkpart primary fat32 1MiB 513MiB
-    sudo parted $LOOP_DEVICE set 1 esp on
-    sudo parted -s $LOOP_DEVICE mkpart primary 513MiB 515MiB
-    sudo parted $LOOP_DEVICE set 2 bios_grub on
-    sudo parted -s $LOOP_DEVICE mkpart primary ext4 515MiB 100%
+    sudo parted -s $USB_NBD mklabel gpt
+    sudo parted -s $USB_NBD mkpart primary fat32 1MiB 513MiB
+    sudo parted $USB_NBD set 1 esp on
+    sudo parted -s $USB_NBD mkpart primary 513MiB 515MiB
+    sudo parted $USB_NBD set 2 bios_grub on
+    sudo parted -s $USB_NBD mkpart primary ext4 515MiB 100%
 
     # Format the partitions
     log "Formatting the partitions..."
-    sudo mkfs.vfat ${LOOP_DEVICE}p1
-    sudo mkfs.ext4 ${LOOP_DEVICE}p3
+    sudo mkfs.vfat ${USB_NBD}p1
+    sudo mkfs.ext4 ${USB_NBD}p3
 
-    sudo e2label ${LOOP_DEVICE}p3 MY_LIVE_SYSTEM
+    sudo e2label ${USB_NBD}p3 MY_LIVE_SYSTEM
 
     sudo mkdir -p $USB_EFI_DIR
     sudo mkdir -p $USB_OS_DIR
-    sudo mount ${LOOP_DEVICE}p1 $USB_EFI_DIR
-    sudo mount ${LOOP_DEVICE}p3 $USB_OS_DIR
+    sudo mkdir -p $RHEL_MNT
+    sudo mount ${USB_NBD}p1 $USB_EFI_DIR
+    sudo mount ${USB_NBD}p3 $USB_OS_DIR
+    # This has to be the second partition. 
+    # The first partition is BIOS BOOT
+    sudo mount ${RHEL_NBD}p2 $RHEL_MNT
 }
 
 # Determine the grub command to use and install GRUB
 install_grub() {
-    if command -v grub2-install &> /dev/null; then
-        GRUB_CMD="grub2-install"
-        log "Currently, we do not support grub2-install due to Red Hat developers."
-        exit 1
-    elif command -v grub-install &> /dev/null; then
-        GRUB_CMD="grub-install"
-    else
-        log "Neither grub2-install nor grub-install found. Please install GRUB for your system."
-        exit 1
-    fi
+    # Going to take grub files from the fedora iso image.
 
-    log "Installing GRUB..."
-    sudo $GRUB_CMD --target=x86_64-efi --no-uefi-secure-boot --efi-directory=$USB_EFI_DIR --boot-directory=$USB_OS_DIR/boot --removable --modules="part_gpt part_msdos"
-    sudo $GRUB_CMD --target=i386-pc --boot-directory=$USB_OS_DIR/boot $LOOP_DEVICE
+    sudo cp /mnt/fedora/EFI/* $USB_EFI_DIR
+    sudo $GRUB_CMD --target=i386-pc --boot-directory=$USB_OS_DIR/boot $USB_NBD
+
 }
 
 create_grub_config() {
@@ -81,27 +107,25 @@ create_grub_config() {
     # Ensure the directory exists
     sudo mkdir -p "$(dirname "$grub_cfg_path")"
 
-    # Write the content to the file
-    sudo bash -c "cat > $grub_cfg_path" << 'EOF'
-set timeout=10
-set default=0
-
-serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
-terminal_input console serial
-terminal_output console serial
-set debug=all
-
-menuentry 'My Custom RHEL8' {
-  search --no-floppy --set=root --label MY_LIVE_SYSTEM
-  linux /vmlinuz rootfstype=squashfs rootflags=loop real_root=/myroot.squashfs console=tty0 console=ttyS0,115200n8 earlyprintk=serial,ttyS0,115200n8 loglevel=7 debug ignore_loglevel
-  initrd /initramfs
+    # Stealing the grub configuration from fedora iso image
+    # and using it for bios mbr grub boot.
+    sudo cp /mnt/fedora/EFI/BOOT/grub.cfg $grub_cfg_path
 }
 
-EOF
+copy_fedora_files() {
+    log "Copying Fedora files..."
+    sudo mkdir -p $USB_OS_DIR/images/pxeboot
+    sudo cp -r /mnt/fedora/isolinux $USB_OS_DIR
+    sudo rm -f $USB_OS_DIR/isolinux/{vmlinuz,initrd.img}
+    VMLINUX=$(find $RHEL_MNT/boot/ -type f -name "vmlinuz*" ! -name "*rescue*" | sort -V | tail -n 1)
+    INITRAMFS=$(find $RHEL_MNT/boot/ -type f -name "initramfs*" ! -name "*rescue*" ! -name "*boot*" | sort -V | tail -n 1)
+    sudo cp "$VMLINUX" $USB_OS_DIR/isolinux/vmlinuz
+    sudo cp "$VMLINUX" $USB_OS_DIR/images/pxeboot/vmlinuz
+    sudo cp "$INITRAMFS" $USB_OS_DIR/isolinux/initrd.img
+    sudo cp "$INITRAMFS" $USB_OS_DIR/images/pxeboot/initrd
+    
 }
 
-# Later in your script, you can simply call:
-# create_grub_config
 
 
 # Final cleanup steps
@@ -109,24 +133,21 @@ final_cleanup() {
     log "Cleaning up..."
     sudo umount $USB_EFI_DIR
     sudo umount $USB_OS_DIR
-    sudo qemu-nbd --disconnect $LOOP_DEVICE
+    sudo qemu-nbd --disconnect $USB_NBD
     sudo modprobe -r nbd
+    sudo umount -l -f /mnt/{fedora,squash,rootfs}
 }
 
 # Main function to coordinate script execution
 main() {
 
-    # Variables
-    LOOP_DEVICE="/dev/nbd0"
-    USB_EFI_DIR="/mnt/virtual_usb_efi"
-    USB_OS_DIR="/mnt/virtual_usb_os"
-    GRUB_CMD=""
-
+    mount_fedora_images
     load_nbd_module
     cleanup
-    setup_loop_device
+    setup_USB_NBD
     install_grub
     create_grub_config
+    copy_fedora_files
     final_cleanup
 
     log "Script completed successfully!"
